fb7d5bfbc1a53c2ec43e87c7dbc2d827208f3262
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-04-27 09:49:45 -0500

    run get for multiple queries

--- server.ts
+++ server.ts
@@ -11,17 +11,18 @@ config(); //Read .env file lines as though they were env vars.
 //For the ssl property of the DB connection config, use a value of...
 // false - when connecting to a local DB
 // { rejectUnauthorized: false } - when connecting to a heroku DB
-const herokuSSLSetting = { rejectUnauthorized: false }
-const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
+const herokuSSLSetting = { rejectUnauthorized: false };
+const sslSetting = process.env.LOCAL ? false : herokuSSLSetting;
 const dbConfig = {
   connectionString: process.env.DATABASE_URL,
   ssl: sslSetting,
 };
 
 const app = express();
+//const pgp = require('pg-promise')();
 
 app.use(express.json()); //add body parser to each following route handler
-app.use(cors()) //add CORS support to each following route handler
+app.use(cors()); //add CORS support to each following route handler
 
 const client = new Client(dbConfig);
 client.connect();
@@ -30,14 +31,36 @@ app.get("/", async (req, res) => {
   res.send("go to endpoint /pastes");
 });
 
+async function getResults() {
+  let results = [];
+  const pastebin_result = await client.query("select * from pastebins");
+  const pastebin_rows = pastebin_result.rows;
+  const summary_result = await client.query("select * from paste_summary");
+  const summary_rows = summary_result.rows;
+  results.push({
+    pastebin: pastebin_rows,
+    summary_result: summary_rows,
+  });
+  return results;
+}
+
 app.get("/pastes", async (req, res) => {
-  const dbres = await client.query('select * from pastebins');
-  res.json(dbres.rows);
+  getResults().then((results) => res.json(results));
 });
 
+/*app.get("/pastes", async (req, res) => {
+  const queries = [{query: "select * from pastebins"}, {query: "select * from paste_summary"}]
+  const sql = pgp.helpers.concat(queries);
+  const dbres = await client.multi(sql)
+  //const dbres = await client.query('select * from pastebins; select * from paste_summary');
+  //const dbres2 = await client.query('select * from paste_summary');
+  res.json(dbres.rows);
+  //res.json(dbres2.rows);
+});*/
+
 app.post("/pastes", async (req, res) => {
-  let text = "insert into pastebins(title, text_body) values ($1, $2) "
-  let values = req.body //=  ["test1", "this is a test to see if it works"]
+  let text = "insert into pastebins(title, text_body) values ($1, $2) ";
+  let values = req.body; //=  ["test1", "this is a test to see if it works"]
   const dbres = await client.query(text, values);
   res.json(dbres.rows);
 });
@@ -45,7 +68,7 @@ app.post("/pastes", async (req, res) => {
 //Start the server on the given port
 const port = process.env.PORT;
 if (!port) {
-  throw 'Missing PORT environment variable.  Set it in .env file.';
+  throw "Missing PORT environment variable.  Set it in .env file.";
 }
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
